441f43439438ea0a02707322a01fb4d8350589e5
christophercodinga11y <christopherjack14@gmail.com>
2025-02-19 14:45:44 -0600

    looks good

--- js/app.js
+++ js/app.js
@@ -17,10 +17,18 @@ class Countries {
         this.form = document.getElementById('countryForm')
 
         this.btn = document.getElementById('submitBtn')
+
+        this.isValid = true
     }
 
     init() {
+        this.isValid = true
+
         this.collectData()
+
+        if (this.isValid) {
+            this.buildCard(this.row, this.country)
+        }
     }
     /**
      * Collect data
@@ -28,9 +36,48 @@ class Countries {
      * Store data
      * Display data
      */
+
+    buildCard(el, obj) {
+        const column = document.createElement('div')
+        column.classList.add('col')
+
+        const card = document.createElement('div')
+        card.classList.add('card', 'h-100')
+        card.setAttribute('id', `${obj.id}`)
+
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+        const countryName = document.createElement('div')
+        countryName.classList.add('card-title', 'text-capitalize')
+        countryName.innerText = obj.countryName
+
+        const capital = document.createElement('p')
+        capital.classList.add('card-text', 'text-capitalize')
+        capital.innerText = `Capital City: ${obj.capital}`
+
+        const stats = document.createElement('p')
+        stats.classList.add('card-text', 'text-capitalize')
+        stats.innerText =  `Population : ${obj.population}| Form of Govt: ${obj.govtType} | Famous Dish: ${obj.cuisine}`
+
+        const cardFooter = document.createElement('footer')
+        cardFooter.classList.add('card-footer')
+
+        const yrEstablished = document.createElement('p')
+        yrEstablished.classList.add('card-text')
+        yrEstablished.innerText = `Year Established: ${obj.yrEstablished}`
+
+        cardFooter.appendChild(yrEstablished)
+        cardBody.appendChild(countryName)
+        cardBody.appendChild(capital)
+        cardBody.appendChild(stats)
+        card.appendChild(cardBody)
+        card.appendChild(cardFooter)
+        column.appendChild(card)
+        el.appendChild(column)
+    }
+
     collectData() {
-        // const form = this.form
-        // const btn = this.btn
 
         const countryName = document.getElementById('countryName').value
         const capital = document.getElementById('capital').value
@@ -48,17 +95,19 @@ class Countries {
             yrEstablished
         }
 
-        this.validateData(population, yrEstablished)
-        this.storeData(obj)
+        this.validateData(obj)
 
     }
+
+
 
     storeData(obj) {
         this.country = {
             id: `${obj.countryName.slice(0, 3)}${obj.yrEstablished}`,
             countryName: obj.countryName,
             capital: obj.capital,
-            population: obj.govtType,
+            population: obj.population.toLocaleString(),
+            govtType: obj.govtType,
             cuisine: obj.cuisine,
             yrEstablished: obj.yrEstablished
         }
@@ -68,11 +117,12 @@ class Countries {
         console.log(this.countries)
     }
 
-    validateData(pop, yr) {
-        if (isNaN(pop) || isNaN(yr)) {
+    validateData(obj) {
+        if (isNaN(obj.population) || isNaN(obj.yrEstablished)) {
             alert('Error: population and year must be numerical values')
+            this.isValid = false
         } else {
-            return
+            this.storeData(obj)
         }
     }
 }
